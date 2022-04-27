661c4f33d7e4d5a25d809e3024ea0dd4d695038e
Jens Willemsens <6514515+JenswBE@users.noreply.github.com>
2022-04-27 23:20:32 +0200

    Simplify firewall configuration

---
+++ helpers/parse_firewall_ports.py
@@ -0,0 +1,14 @@
+import json
+import sys
+
+ports = json.loads(sys.argv[1])
+output = []
+for port in ports:
+    for proto in port['protos']:
+        for from_network in port['from_networks']:
+            output.append({
+                'port': port['port'],
+                'proto': proto,
+                'from_network': from_network,
+            })
+print(json.dumps(output))
