f6cfcfcf4e26a8af35c7d6b81ecd6a9eb055ef4b
JenswBE <6514515+JenswBE@users.noreply.github.com>
2024-07-20 10:24:01 +0200

    rename_date: Replace spaces with underscore

--- scripts/bash/rename_date.sh
+++ scripts/bash/rename_date.sh
@@ -15,12 +15,12 @@ APPENDIX="${2}"
 OTHER_EXT=("png" "mp4")
 
 # Rename JPEG accordingly to EXIF
-jhead -autorot -nf"${PREFIX}%Y-%m-%d %H-%M-%S %03i${APPENDIX}" *.jpg
+jhead -autorot -nf"${PREFIX}%Y-%m-%d_%H-%M-%S_%03i${APPENDIX}" *.jpg
 
 # Rename other files accordingly to modify date
 for ext in "${OTHER_EXT[@]}"; do
   for file in *.${ext}; do
-    NEW_FILENAME=$(stat "${file}" --format %y | cut -c -19 | tr : -)
+    NEW_FILENAME=$(stat "${file}" --format %y | cut -c -19 | tr : - | tr ' ' _)
     mv "${file}" "${PREFIX}${NEW_FILENAME}${APPENDIX}.${ext}"
   done
 done
