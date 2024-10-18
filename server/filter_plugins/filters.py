71e2051e31c71b87cdddf02f1b240951b7d23d9a
JenswBE <6514515+JenswBE@users.noreply.github.com>
2024-10-18 07:53:18 +0200

    Server: Use internal DNS instead of Wireguard hostnames

--- server/filter_plugins/filters.py
+++ server/filter_plugins/filters.py
@@ -7,23 +7,8 @@ class FilterModule(object):
 
     def filters(self):
         return {
-            'parse_ufw_firewall_ports': self.parse_ufw_firewall_ports,
             'to_toml': self.to_toml,
         }
-
-
-    def parse_ufw_firewall_ports(self, ports):
-        output = []
-        for port in ports:
-            for proto in port['protos']:
-                for from_network in port['from_networks']:
-                    output.append({
-                        'comment': port['comment'],
-                        'port': port['port'],
-                        'proto': proto,
-                        'from_network': from_network,
-                    })
-        return output
 
     # Based on https://www.iops.tech/blog/generate-toml-using-ansible-template/
     def to_toml(self, input):
