ce66f6f9da7be018f5388ff699c651016aeb4064
JenswBE <6514515+JenswBE@users.noreply.github.com>
2026-06-07 13:07:52 +0000

    Grafana: Migrate deprecations query from Graylog

--- server/files/graylog-iac/lint.sh
+++
@@ -1,2 +0,0 @@
-# This script is intended to be sourced like "source lint.sh" or ". lint.sh".
-golangci-lint run --config ../../../programming_configs/golang/.golangci.yml --path-prefix "files/graylog-iac"
