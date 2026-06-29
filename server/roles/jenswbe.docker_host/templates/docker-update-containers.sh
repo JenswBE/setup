d78971cfd49979b6aa9dc18d137dc197c9cdcecb
JenswBE <6514515+JenswBE@users.noreply.github.com>
2026-06-29 17:21:40 +0000

    Dockre update script: Improve logging

--- server/roles/jenswbe.docker_host/templates/docker-update-containers.sh
+++ server/roles/jenswbe.docker_host/templates/docker-update-containers.sh
@@ -5,12 +5,17 @@ set -euo pipefail
 
 # Update Docker containers
 cd '{{ ansible_facts["user_dir"] }}/deploy'
-ec_pull=0;  docker compose --profile 'scheduled' pull         || ec_pull=$?
-ec_build=0; docker compose --profile 'scheduled' build --pull || ec_build=$?
-ec_up=0;    docker compose up -d                              || ec_up=$?
+echo "=== docker compose pull ==="
+ec_pull=0;  docker compose --progress plain --profile 'scheduled' pull         || ec_pull=$?
+echo "=== docker compose build ==="
+ec_build=0; docker compose --progress plain --profile 'scheduled' build --pull || ec_build=$?
+echo "=== docker compose up ==="
+ec_up=0;    docker compose --progress plain up -d                              || ec_up=$?
 
 # Prune unused Docker resources
+echo "=== docker image prune ==="
 ec_image_prune=0;  docker image prune -f  || ec_image_prune=$?
+echo "=== docker buildx prune ==="
 ec_buildx_prune=0; docker buildx prune -f || ec_buildx_prune=$?
 
 # Print overview
