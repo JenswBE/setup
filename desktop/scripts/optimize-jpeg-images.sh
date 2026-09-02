ab91022516a0c12abd413261103848ccc0f2de81
JenswBE <6514515+JenswBE@users.noreply.github.com>
2026-09-02 17:42:48 +0000

    Fix shellcheck error

--- desktop/scripts/optimize-jpeg-images.sh
+++ desktop/scripts/optimize-jpeg-images.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 jpegoptim --strip-all --all-progressive --threshold=5 --max=80
--- server/graylog.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Bash strict mode (http://redsymbol.net/articles/unofficial-bash-strict-mode/)
-set -euo pipefail
-
-cd files/graylog-iac
-go run ./...
